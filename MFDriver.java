27b97d72409dae54109ee686e6ba2d84d2b86888
Lynne Wang <lwang7@stuy.edu>
2018-02-09 07:17:19 -0500

    Changed Driver

--- MFDriver.java
+++ MFDriver.java
@@ -17,36 +17,14 @@ public class MFDriver {
 	        MatrixFinder.alg( testA, testA[0][testA.length - 1] );
 		
 	        long timeFinal = System.nanoTime();
-		long timeOne = (timeFinal - timeInit)/20;
+		long timeOne = (timeFinal - timeInit);
 		sum += timeOne;
 		//System.out.print(a + "," + timeOne + "\n");
+
 	    }
 	    System.out.print(a + "," + (sum / 20.) + "\n");
 	}
 	
-	/*
-	  int var = 0;
-	  int[][] testB = new int[20000][20000];
-	  for (int i = 0; i < testB.length; i++)
-	  for (int x = 0; x < testB.length; x++) {
-	  testB[i][x] = var;
-	  var++;
-	  }
-
-	  for (int i = 0; i < 100; i++) {
-	  System.out.println("testB Results:");
-	  double timeInit = System.currentTimeMillis();
-		
-	  for(int n = 0; n < 10; n++){
-	  MatrixFinder.alg( testB, testB[0][testB.length - 1]);
-	  }
-		
-	  double timeFinal = System.currentTimeMillis();
-	  System.out.println("Total Time: " + (timeFinal - timeInit));
-	  double timeTotal = (timeFinal - timeInit)/10;
-	  System.out.println("Time for One Search: " + timeTotal + "\n");
-	  }
-	*/
     }//end main
 
 }
